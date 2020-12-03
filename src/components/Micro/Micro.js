dd3019cda556e7dfa023a3c72ee0aeb389e4f086
Julián Montoya <jc.montoyar@uniandes.edu.co>
2020-12-03 11:00:53 -0500

    seciruty and micro completed

--- src/components/Micro/Micro.js
+++ src/components/Micro/Micro.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/nav";
 import Content from "../Content/Content";
+import "./Linter.css"
 
 //Imgs
 import Picture1 from "./Picture1.png";
@@ -15,7 +16,7 @@ import Dark2 from "./dark2.jpg";
 import Light from "./light1.jpg";
 import PerformanceIssue from "./performance.png";
 import Size from "./size.jpg";
-
+import MicroImg from "./Micro.png"
 
 const items = [
   "Unnecesary objects",
@@ -27,7 +28,7 @@ const items = [
   "Linter",
   "Overdrawing",
   "APK and bundle size",
-  "Unused resources"
+  "Unused resources",
 ];
 
 const contenido = [
@@ -35,122 +36,149 @@ const contenido = [
     name: items[0],
     contenido: [
       {
-        text: "After going through the main classes of the code we found 0 Kotlin or Java warnings of variables not being used, and also after analyzing the loops used on the code, there is no sign of auxiliary variables being created inside of the loops. There was also no warning on the linter analysis regarding this type of micro optimization.",
+        text:
+          "After going through the main classes of the code we found 0 Kotlin or Java warnings of variables not being used, and also after analyzing the loops used on the code, there is no sign of auxiliary variables being created inside of the loops. There was also no warning on the linter analysis regarding this type of micro optimization.",
         image: Picture1,
         alt: "Image of an unnecesary item",
         h: "540px",
         w: "300px",
-        titulo: "Creating unnecessary objects on Code and on drawing Views."
-      }
+        titulo: "Creating unnecessary objects on Code and on drawing Views.",
+      },
     ],
   },
   {
     name: items[1],
     contenido: [
       {
-        text: "	In this point the app could see some improvement since it is overusing the class Integer instead using primitive int. We found over 34 classes that use Integer object for handling attributes that could be easily replaced with int.   The same goes for Long which includes over 40 classes where the object Long is used instead of primitive long.",
+        text:
+          "	In this point the app could see some improvement since it is overusing the class Integer instead using primitive int. We found over 34 classes that use Integer object for handling attributes that could be easily replaced with int.   The same goes for Long which includes over 40 classes where the object Long is used instead of primitive long.",
         image: Picture1,
         alt: "Image of a primitive being",
         h: "540px",
         w: "300px",
         titulo: "Using primitive types over wrappers",
-      }
+      },
     ],
   },
   {
     name: items[2],
     contenido: [
       {
-        text: "Even though they use primarily indexed loops, they are overusing the for each method in some classes. In some other few cases they are using iterator of keys and values that could also be replaced with indexed loops in order to improve performance.",
+        text:
+          "Even though they use primarily indexed loops, they are overusing the for each method in some classes. In some other few cases they are using iterator of keys and values that could also be replaced with indexed loops in order to improve performance.",
         image: For,
         alt: "Image of the loops using for each or iterator",
         h: "800px",
         w: "140px",
         titulo: "Indexed loops instead of iterators or for each’s",
-      }
+      },
     ],
   },
   {
     name: items[3],
     contenido: [
       {
-        text: "As we explained on the caching section of the report, Habit tracker uses for the most part Linked Lists. Since this is a non-memory efficient data structure, we would recommend replacing all Linked lists with ArrayMaps. This data structure would improve the memory usage without compromising the response speed time that Linked lists provide currently to the app.",
+        text:
+          "As we explained on the caching section of the report, Habit tracker uses for the most part Linked Lists. Since this is a non-memory efficient data structure, we would recommend replacing all Linked lists with ArrayMaps. This data structure would improve the memory usage without compromising the response speed time that Linked lists provide currently to the app.",
         image: Hash,
         alt: "Image of an Array Map",
         h: "500px",
         w: "210px",
         titulo: "Memory friendly data structures",
-      }
+      },
     ],
   },
   {
     name: items[4],
     contenido: [
       {
-        text: "This android method is only used 13 times (on 7 different activities) in the whole android Habit tracker project, for that reason it would not be recommended to make code changes to move to a binding pattern (it would not be worth it since it has very low usage).",
+        text:
+          "This android method is only used 13 times (on 7 different activities) in the whole android Habit tracker project, for that reason it would not be recommended to make code changes to move to a binding pattern (it would not be worth it since it has very low usage).",
         image: Lupa,
         alt: "Image of android icon",
         h: "300px",
         w: "210px",
         titulo: "Avoiding the use of the find by id method in Android",
-      }
+      },
     ],
   },
   {
     name: items[5],
     contenido: [
       {
-        text: "We found 7 different layouts with nested linear layouts, you can check the list at the right image. In order to make the application more optimal Habit tracker would need to replace those layouts using constrained or relative ones.",
+        text:
+          "We found 7 different layouts with nested linear layouts, you can check the list at the right image. In order to make the application more optimal Habit tracker would need to replace those layouts using constrained or relative ones.",
         image: Layout,
         alt: "Image of an Android layout",
         h: "400px",
         w: "210px",
-        titulo: "Using relative or constraint layouts instead of nested linear layouts",
-      }
+        titulo:
+          "Using relative or constraint layouts instead of nested linear layouts",
+      },
+    ],
+  },
+  {
+    name: items[6],
+    contenido: [
+      {
+        text:
+          "	After running the default linter analysis using android studio, we found 201 errors and 312 warnings. Speaking of the ones related to performance, 2 of them have a 6/10 priority score and the rest are scored with a 3 or a 1. Even though it seems there is not a significant warning, there are 99 warnings that together could translate into potential performance bugs.",
+        image: MicroImg,
+        alt: "Image of the linter analysis",
+        h: "700px",
+        w: "1000px",
+        titulo:
+          "Lint Report",
+      },
     ],
   },
   {
     name: items[7],
     contenido: [
       {
-        text: "As shown in the linter analysis, there are two overdraw warnings. This can be found on the about.xml and the show_habt_inner.xml. We decided to run the app while using the android’s GPU overdraw in order to check if the linter had missed some other overdrawn areas in the application. To the right to can check the specific color meanings from the Android’s GPU overdraw utility.",
+        text:
+          "As shown in the linter analysis, there are two overdraw warnings. This can be found on the about.xml and the show_habt_inner.xml. We decided to run the app while using the android’s GPU overdraw in order to check if the linter had missed some other overdrawn areas in the application. To the right to can check the specific color meanings from the Android’s GPU overdraw utility.",
         image: GPU,
         alt: "Image with leyends of the Android’s GPU overdraw utility",
         h: "300px",
         w: "250px",
-        titulo: "Avoid overdrawing"
+        titulo: "Avoid overdrawing",
       },
       {
         text: "About and settings activities using dark theme",
         image: Dark1,
-        alt: "Image of about and setting activities using dark theme with GPU overdraw utility enabled",
+        alt:
+          "Image of about and setting activities using dark theme with GPU overdraw utility enabled",
         h: "500px",
         w: "610px",
-        titulo: "Dark Theme"
+        titulo: "Dark Theme",
       },
       {
         text: "Habits list and habit detail activities using dark theme",
         image: Dark2,
-        alt: "Image of Habits list and habit detail activities using dark theme with GPU overdraw utility enabled",
+        alt:
+          "Image of Habits list and habit detail activities using dark theme with GPU overdraw utility enabled",
         h: "500px",
         w: "610px",
-        titulo: "Dark Theme part 2"
+        titulo: "Dark Theme part 2",
       },
       {
         text: "Habits list and habit detail activities using light theme",
         image: Light,
-        alt: "Image of Habits list and habit detail activities using light theme with GPU overdraw utility enabled",
+        alt:
+          "Image of Habits list and habit detail activities using light theme with GPU overdraw utility enabled",
         h: "500px",
         w: "610px",
-        titulo: "Light Theme"
+        titulo: "Light Theme",
       },
       {
-        text: "Running the application with the GPU overdraw tool activated clearly shows that the app is overdrawing a lot, this could be causing performance issues or simply making the performance worse than it could be. This is more significant using dark mode, where the overdraw happens up to 4 times or more in the detail habit activity and in the list of habits.",
+        text:
+          "Running the application with the GPU overdraw tool activated clearly shows that the app is overdrawing a lot, this could be causing performance issues or simply making the performance worse than it could be. This is more significant using dark mode, where the overdraw happens up to 4 times or more in the detail habit activity and in the list of habits.",
         image: PerformanceIssue,
         alt: "Bad performance icon",
         h: "500px",
         w: "210px",
-        titulo: "Overview"
+        titulo: "Overview",
       },
     ],
   },
@@ -158,26 +186,28 @@ const contenido = [
     name: items[8],
     contenido: [
       {
-        text: "Habit tracker has a very small APK size, weighing roughly 3.4 MB. Also you can see that they have made a big effort in reducing bundle size as you can check that they have built their own pickers (time, color, calendar, etc.) instead of relying on third party packages. Also after installing and running the google’s app size analyser plug in, the results show the most optimal output, showing 0 recommendations in order to reduce the app’s size",
+        text:
+          "Habit tracker has a very small APK size, weighing roughly 3.4 MB. Also you can see that they have made a big effort in reducing bundle size as you can check that they have built their own pickers (time, color, calendar, etc.) instead of relying on third party packages. Also after installing and running the google’s app size analyser plug in, the results show the most optimal output, showing 0 recommendations in order to reduce the app’s size",
         image: Size,
         alt: "Image of the result of the android size analyser plug in",
         h: "800px",
         w: "110px",
         titulo: "Reducing APK and bundle size",
-      }
+      },
     ],
   },
   {
     name: items[9],
     contenido: [
       {
-        text: "As shown in the Linter analysis, there are 70 unused resources in the whole application. These are 70 small fix changes that could be easily done, and while doing so you would be improving the performance, as well as the memory usage of the application.",
+        text:
+          "As shown in the Linter analysis, there are 70 unused resources in the whole application. These are 70 small fix changes that could be easily done, and while doing so you would be improving the performance, as well as the memory usage of the application.",
         image: Picture1,
         alt: "Image of a linter cleaning code",
         h: "540px",
         w: "300px",
         titulo: "Removing unused resources",
-      }
+      },
     ],
   },
 ];
@@ -214,10 +244,19 @@ export default function Micro() {
           buildProfilerSetting(contenido.name, curNav, contenido.contenido)
         )}
       </div>
-      <hr/>
+      <hr />
       <h2 className="title text-center  ">Overview</h2>
       <div className="container">
-        <h3 className="alignTextCenter">Overall habit tracker has many places where it could do more in order to optimize its product, especially fixing overdrawing, removing some unused resources, using primitive types over objects and replacing nested linear layouts with constrained or relative ones. But in the other hand, the app makes a great work in its APK and bundle size, usage of weak references in multithreading, use of static in nested classes, good practices in lifecycle methods and very limited usage of the find view by id method.</h3>
+        <h3 className="alignTextCenter">
+          Overall habit tracker has many places where it could do more in order
+          to optimize its product, especially fixing overdrawing, removing some
+          unused resources, using primitive types over objects and replacing
+          nested linear layouts with constrained or relative ones. But in the
+          other hand, the app makes a great work in its APK and bundle size,
+          usage of weak references in multithreading, use of static in nested
+          classes, good practices in lifecycle methods and very limited usage of
+          the find view by id method.
+        </h3>
       </div>
     </div>
   );
--- src/components/Security/Security.js
+++ src/components/Security/Security.js
@@ -1,9 +1,144 @@
-import React from "react"
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/nav";
+import Content from "../Content/Content";
 
-export default function Security(){
-    return(
-        <div className="container-fluid">
-            <h1 className="text-center">Security</h1>
-        </div>
-    );
+//Imgs
+import Intent from "./intent.png";
+import AndroidSec from "./androidsec.png";
+import Log from "./log.png";
+import Settings from "./settings.png";
+import Manifest from "./manifest.jpg";
+import DataLeak from "./leak.jpg";
+
+const items = ["Intents", "Data", "Exported components"];
+
+const contenido = [
+  {
+    name: items[0],
+    contenido: [
+      {
+        text:
+          "Habit tracker has one class than handles all the intents of the application (IntentFactory.kt). Even though most of them are explicit within the same application, there are 6 different implicit intents that mostly redirect to webpages with different information helpers of the app (FAQ, github repository, email, playstore, etc). These do not represent a direct security tread, however the user might without knowing decide to open the web page or email with an untrusted application, and therefore creating a possible security issue.",
+        image: Intent,
+        alt: "Image of an Android intent",
+        h: "500px",
+        w: "300px",
+        titulo: "Implicit intents",
+      },
+    ],
+  },
+  {
+    name: items[1],
+    contenido: [
+      {
+        text:
+          "Each habit and its related data are stored in the local SQLite database on the phone’s local storage. On a first analysis this would mean that this data is safe, however since Android still has a long way to go in order to make local storage fully secured, it’s possible for another malicious app to force its way to the SQLite database and get those habits data from the user. This could be fixed using SQLCipher or using the SQLite version that secures the data (however this would need payment which probably would harm the current business model that Loop habit tracker has). ",
+        image: AndroidSec,
+        alt: "Image of android security",
+        h: "500px",
+        w: "300px",
+        titulo: "Data issues",
+      },
+      {
+        text:
+          "Regarding the logs, the app has 60 of them throughout the whole project, however its purpose is mostly for performance profiling, debugging, checking the status of systems, etc.  So, there is not any sensitive data getting leaked through there.",
+        image: Log,
+        alt: "Image of a log",
+        h: "300px",
+        w: "300px",
+        titulo: "Logs",
+      },
+      {
+        text:
+          "Even though the shared preferences are not encrypted using the EncryptedSharedPreferences class, we do not consider this to be a security issue, since the data stored using this type of local storage are related to very basic settings that do not represent anything sensible or private.",
+        image: Settings,
+        alt: "Image of  the settings icon",
+        h: "300px",
+        w: "300px",
+        titulo: "Shared preferences",
+      },
+    ],
+  },
+  {
+    name: items[2],
+    contenido: [
+      {
+        text:
+          "In the app’s manifest can be found 9 activities, 8 receivers and 1 provider. Only 2 activities and 1 receiver have the android exported attribute set as true, mainly to manage notifications, email, and widget related functionalities. Following good practices, the provider has explicitly set the same attribute to false, however the rest of activities and receivers could improve security practices by setting the attribute as false as well (It is not explicit).",
+        image: Manifest,
+        alt: "Image of a manifest",
+        h: "500px",
+        w: "300px",
+        titulo: "Logs",
+      },
+      {
+        text:
+          "One of the activities that can be exported, is the one that handles the list of habits, so the data issues that were spoken about in the data sub-section, get much more problematic and impose a significant issue to the user’s data (Even though there is no personal information such as name, age, etc;  Your habits could be accessed from a third party that could sell or use that data without the user finding out).",
+        image: DataLeak,
+        alt: "Leak",
+        h: "400px",
+        w: "500px",
+        titulo: "Security issue regarding the data",
+      },
+    ],
+  },
+];
+
+export default function Security() {
+  const [curNav, setCurNav] = useState(items[0]);
+  const handleChange = (newNav) => {
+    setCurNav(newNav);
+  };
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <h1 className="text-center">Security</h1>
+        <Navbar expand="lg" className="">
+          <Navbar.Toggle aria-controls="basic-navbar-nav-subContent" />
+          <Navbar.Collapse id="basic-navbar-nav-subContent">
+            <Nav className="mx-auto">
+              {items.map((item) => (
+                <Nav.Link
+                  onClick={() => handleChange(item)}
+                  className={curNav === item ? "activeSubNav" : " navContent"}
+                  key={"subnav" + item}
+                >
+                  {item}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      <hr className="mt-0 mb-0 hrNav" />
+      <div className="contenidosSub">
+        {contenido.map((contenido) =>
+          buildProfilerSetting(contenido.name, curNav, contenido.contenido)
+        )}
+      </div>
+      <hr />
+      <h2 className="title text-center  ">Overview</h2>
+      <div className="container">
+        <h3 className="alignTextCenter">
+          Even though Loop habit tracker tries to follows good security
+          practices, it is not extent of security threads, specially looking at
+          the habits data, which not only suffers from the common Android’s
+          local storage security issues, but also since the activities that
+          handle that data are exported, it makes it much easier for malicious
+          apps to access that data with out the user ever finding out.
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+function buildProfilerSetting(name, curNav, contenido) {
+  let style = name === curNav ? "" : "none";
+
+  return (
+    <div style={{ display: style }}>
+      <Content contenidos={contenido}></Content>
+    </div>
+  );
 }
